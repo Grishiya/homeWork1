2bb732eeec6d634fb53ff63da28bd4d0db627cb2
Grisha <guruwolf8@gmail.com>
2023-06-13 19:54:02 +0300

    Объекты)мне нравится,ctrl+alt+l. Поменял имя Author paolini.

--- src/pro/sky/java/course1/objects/Objects.java
+++ src/pro/sky/java/course1/objects/Objects.java
@@ -4,10 +4,10 @@ import java.util.Arrays;
 
 public class Objects {
     public static void main(String[] args) {
-        Author Eragon = new Author("Кристофер ", "Паолини, ");
-        System.out.println("Eragon = " + Eragon.getNameAuthor() + Eragon.getSurnameAuthor());
-        Book eragon = new Book(Eragon, "Эрагон, ", 2003);
-        System.out.println("eragon = " + eragon.getBookName() + Eragon.getNameAuthor() + Eragon.getSurnameAuthor() + eragon.getYearOfPublication());
+        Author  paolini = new Author("Кристофер ", "Паолини, ");
+        System.out.println("Eragon = " + paolini.getNameAuthor() + paolini.getSurnameAuthor());
+        Book eragon = new Book(paolini, "Эрагон, ", 2003);
+        System.out.println("eragon = " + eragon.getBookName() + paolini.getNameAuthor() + paolini.getSurnameAuthor() + eragon.getYearOfPublication());
         Author oda = new Author("Эйтиро ", "Ода, ");
         Book onePiece = new Book(oda, "One Piece, ", 1998);
         System.out.println("onePiece = " + onePiece.getBookName() + oda.getNameAuthor() + oda.getSurnameAuthor() + onePiece.getYearOfPublication());
