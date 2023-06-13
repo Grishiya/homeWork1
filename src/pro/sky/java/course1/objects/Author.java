4069b488cea6635b42944cf79f341f4be96332c8
Grisha <guruwolf8@gmail.com>
2023-06-13 18:57:38 +0300

    Объекты)мне нравится

---
+++ src/pro/sky/java/course1/objects/Author.java
@@ -0,0 +1,23 @@
+package pro.sky.java.course1.objects;
+
+public class Author {
+    private String nameAuthor;
+    private String surnameAuthor;
+    public Author (String nameAuthor,String surnameAuthor){
+        this.nameAuthor=nameAuthor;
+        this.surnameAuthor=surnameAuthor;
+    }
+    public String getNameAuthor(){
+        return this.nameAuthor;
+    }
+
+    public String getSurnameAuthor() {
+        return this.surnameAuthor;
+    }
+    public void setNameAuthor(String nameAuthor){
+        this.nameAuthor=nameAuthor;
+    }
+    public void setSurnameAuthor(String surnameAuthor){
+        this.surnameAuthor = surnameAuthor;
+    }
+}
--- src/pro/sky/java/course1/objects/Objects.java
+++ src/pro/sky/java/course1/objects/Objects.java
@@ -1,8 +1,17 @@
 package pro.sky.java.course1.objects;
 
-public class Objects {
-    public static void main (String[] args){
-        System.out.println("хай");
+import java.util.Arrays;
 
+public class Objects {
+    public static void main(String[] args) {
+        Author Eragon = new Author("Кристофер ", "Паолини ");
+        System.out.println("Eragon = " + Eragon.getNameAuthor() + Eragon.getSurnameAuthor());
+        Book eragon = new Book(Eragon, "Эрагон ", 2003);
+        System.out.println("eragon = " + eragon.getBookName() + Eragon.getNameAuthor() + Eragon.getSurnameAuthor() + eragon.getYearOfPublication());
+        Author oda = new Author("Эйтиро ", "Ода ");
+        Book onePiece=new Book(oda,"One Piece ",1998);
+        System.out.println("onePiece = " + onePiece.getBookName()+oda.getNameAuthor()+oda.getSurnameAuthor()+onePiece.getYearOfPublication());
+        eragon.setYearOfPublication(2002);
+        System.out.println("eragon = " + eragon.getYearOfPublication());
     }
 }
